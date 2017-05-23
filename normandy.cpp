52a4b01787ffeadc54e390e74c3732faa4c01e43
Kenneth-Hudgins <kennethhudgins21@gmail.com>
2017-05-22 23:13:13 -0500

    Added Normandy ascii art

--- normandy.cpp
+++ normandy.cpp
@@ -73,6 +73,20 @@ intro();
 
 void intro(){
 
-	cout << " ASCI NORMANDY TEXT ART GOES HERE" << endl;
+
 	cout << "\n\n\n";
+	cout << "                                                         888         " << endl;
+	cout << "                                                         888         " << endl;
+	cout << "                                                         888         " << endl;
+	cout << "88888b.  .d88b. 888d88888888b.d88b.  8888b. 88888b.  .d88888888  888 " << endl;
+	cout << "888 '88bd88''88b888P'  888 '888 '88b    '88b888 '88bd88' 888888  888 " << endl;
+	cout << "888  888888  888888    888  888  888.d888888888  888888  888888  888 " << endl;
+	cout << "888  888Y88..88P888    888  888  888888  888888  888Y88b 888Y88b 888 " << endl;
+	cout << "888  888 'Y88P' 888    888  888  888'Y888888888  888 'Y88888 'Y88888 " << endl;
+	cout << "                                                                 888 " << endl;
+	cout << "                                                            Y8b d88P " << endl;
+	cout << "                                                             'Y88P'  " << endl;
+
+
+
 }
