893c323a62c3458bde15f2dd610f66b354a62b65
Kenneth-Hudgins <kennethhudgins21@gmail.com>
2017-05-25 23:36:08 -0500

    Finished intro, started story segment 01

--- normandy.cpp
+++ normandy.cpp
@@ -2,78 +2,76 @@
 #include <iostream>
 #include <ctime>
 #include <string>
-
 #include <cstdlib>
 #include "characters.hpp"
 
 using namespace std;
 
-void intro();
+void intro(characters *d);
 
-//Will need to have random number generator inside the function
+void story_segment01(characters *d);
+
 characters random_player();
 
 
-
+//################################################*************************
+//################## BEGIN MAIN ##################*************************
+//################################################*************************
 int main(){
 
-
+/*
+Thought about how I could just have
+the pointer accept the address but after
+a few minuets of messing with it I stopped
+because the below already works
+ */
 characters player = random_player();
-
+characters *ptr = &player;
 
 //While loop control
 bool done = false;
-
 //Players menu choice
 char choice;
-
 //Players miles traveled
 int miles_to_pill = 0;
-
 //Players thirst level
 int thirst = 0;
-
 //Camels tiredness level
 int ammo = 0;
-
 //Amount of drinks left in canteen
 int canteen = 3;
 
+//Players health
 int health = 100;
+
+
 
 
 random_player();
 
+intro(ptr);
 
-intro();
-
+story_segment01(ptr);
 
 	return 0;
 
 }
 
 
-//#########################################################################
-
-//################## FUNCTIONS BELOW ######################################
-
-//#########################################################################
-
-
-
-
+//################################################***********************
+//################## END MAIN ####################***********************
+//################################################***********************
 
 
 
 
 //#################################################
-
 //################## INTRO FUNCTION ###############
-
 //#################################################
-void intro(){
+void intro(characters *d){
 
-
+//Choice variable only used for this function
+char choice = ' ';
 
 
 
@@ -90,9 +88,34 @@ void intro(){
 	cout << "                                                                 Y8b d88P " << endl;
 	cout << "                                                                  'Y88P' \n\n\n " << endl;
 
+//Pause 
+cin.get();
 
+//Input validation
+do{
+
+cout << "Continue? \n Y / N:" << endl;
+cin >> choice;
+
+}while(choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N');
+if (choice == 'n' || choice == 'N'){
+	cout << "Program Terminated." << endl;
+	exit(0);
+}
+
+
+//Displays the characters backstory so they have a sense of who
+//they are playing as.
+		cout << "\n####################################" << endl;
+		cout << "#         PLAYER CHARACTER         #" << endl;
+		cout << "####################################\n" << endl;
+		cout << d->get_backstory() << endl;
+		cout << "####################################" << endl;
+		cout << "#                                  #" << endl;
+		cout << "####################################\n\n\n" << endl;
 		
-
+		//Pause
+		cin.get();
 /*
 The above ascii art was taken from: 
 http://normandy.ascii.uk/ 
@@ -103,17 +126,14 @@ Had to change all of the " to '.
 
 
 //######################################################
-
 //################## CHARACTERS FUNCTION ###############
-
 //######################################################
 characters random_player(){
 
+
+//Random number generator from 0-2
 srand(time(NULL));
-
 		int random_num =(rand()% (2-0+1))+0;
-
-
 
 characters soldier[3];
 
@@ -122,13 +142,13 @@ characters soldier[3];
 //how to make word wrap work without compile time errors
 
 //Character 1 backstory
-string bs00 = "Your name is John Hamstaff. Born and raised in Austin, Texas; you’re a laid-back\ncity man through and through. Life was okay back home in the city, but when your\nnumber came up in the draftyou didn’t care all that much. There wasn’t much\nholding you down in Austin since most of the local guys had been drafted over\nthe last few months. You knew your number would come up eventually. You not\nparticularly upset about it.\n\n";
+string bs00 = "Your name is John Hamstaff. Born and raised in Austin, Texas; you’re a laid-back\ncity man through and through. Life was okay back home in the city, but when your\nnumber came up in the draftyou didn’t care all that much. There wasn’t much\nholding you down in Austin since most of the local guys had been drafted over\nthe last few months. You knew your number would come up eventually. You not\nparticularly upset about it.\n";
 
 //Character 2 backstory
-string bs01 = "Your name is Samuel Suthers. You were once a pastor of the St Marks United\nMethodist Church in Murfreesboro, Tennesse. Although you are not a violent\nman by any means you knew perfectly well that the lord had finally called\napon you to servein his righteous army. Your a god fearing man, and if it\nwas his will that led to your draft number being called then so be it.\n\n";
+string bs01 = "Your name is Samuel Suthers. You were once a pastor of the St Marks United\nMethodist Church in Murfreesboro, Tennesse. Though never one to join the \nservice on your own you knew perfectly well that the lord had finally\ncalled apon you to serve in his army the moment you opened your door to\nsee the face of the local recruiter. Your a god fearing man, and if it\nwas his will that led to your draft number being called then so be it.\n";
 
 //Character 3 backstory
-string bs02 = "Your name is Ramirez Vargas. Your grandparents immigrated to the United States\n30 years ago after your parents died of a freak accident when a train managed\nto somehow jump the tracks on a hill next to their house and then plowed into\nit, killing both of them while you were away at the nursery. The fact the the\ntrain just so happen to be manufactured by the same company that now makes\nweapons for the nazis probably had a little to do with your enlistment.\n\n";
+string bs02 = "Your name is Ramirez Vargas. Your grandparents immigrated to the United States\n30 years ago after your parents died of a freak accident when a train managed\nto somehow jump the tracks on a hill next to their house and then plowed into\nit, killing both of them while you were away at the nursery. The fact the the\ntrain just so happen to be manufactured by the same company that now makes\nweapons for the nazis probably had a little to do with your enlistment.\n";
 
 
 soldier[0].set("John", "Hamstaff", "Hammy", bs00);
@@ -138,6 +158,27 @@ soldier[1].set("Samuel", "Suthers", "Preacher", bs01);
 soldier[2].set("Ramirez", "Vargas", "Vargo", bs02);
 
 
-
 return soldier[random_num];
+}
+
+
+
+
+//############################################################
+//################## STORY SEGMENT 01 FUNCTION ###############
+//############################################################
+void story_segment01(characters *d){
+		cout << "####################################" << endl;
+		cout << "#            The beach             #" << endl;
+		cout << "####################################\n" << endl;
+	
+
+
+
+
+		cout << "####################################" << endl;
+		cout << "#                                  #" << endl;
+		cout << "####################################\n\n\n" << endl;
+
+
 }
